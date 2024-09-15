3e484a56508df274b9a9a31af17da7ce3e72966e
SoftwareTidbits <softwaretidbits@gmail.com>
2024-09-15 09:27:29 -0500

    Part I examples

--- ell-1/README.md
+++ ell-1/README.md
@@ -1,9 +1,97 @@
-# Software Tidbits ELL - Part 1
+# Software Tidbits Ell - Part 1
 
-Reference repositories:
-* https://github.com/MadcowD/ell
-* https://github.com/softwaretidbits/ell
+## About
+Ell is a lightweight prompt engineering library treating prompts as functions. Prompts are thought of as programs and not strings with ell. 
+
+Thinking in terms of functions instead of individual strings, LMPs (language model programs) represent functions where a set of functions makeup a program.
+
+The program itself encapsulates either a string prompt or a list of messages to be sent to various multimodal language models.
+
+This is the gist of ell.
+
+## Language Model Programs
+
+LMPs represent functions. Using ell's decorators (as decorators in your Python logic) you can convert a regular Python function into an LMP.
+
+Below is an example of ell's simple decorator used to work with language models that return text.  
+
+```python
+ell.simple(model: str, client: Any | None = None, exempt_from_tracking=False, **api_params)
+```
+
+LLMs can process and generate various types of content (text, audio, video, and images). As such, there is a decorator (called complex) for this as well.  Let's take a look.
+
+```python
+ell.complex(model: str, client: Any | None = None, exempt_from_tracking=False, tools: List[Callable] | None = None, post_callback: Callable | None = None, **api_params)
+```
+
+There are many capabilities when using the complex decorator.  
+
+An example of how the complex decorator supports structured outputs is below. 
+
+### Want to retain your versioned prompts for future usage?
+
+```python
+ell.init(store='./prompts', autocommit=True)
+```
+
+You can explore your prompts using **ell-studio**.
+
+ell-studio is installed by default, just run it!
+
+```bash
+ell-studio --storage ./prompts
+```
+
+Now you can visualize all of your LMPs saved in the ./prompts directory. Track the changes over time and most importantly inspect their dependencies. Ell will serialize the arbitrary Python code allowing you to track how the LMPs are used through their outputs and inputs. 
+
+Actually making something of higher quality using AI requires construction involving numerous calls to an LLM.  That is prompt engineering.  Using functions breaks down the problem and makes it easier to retain and implement foundational techniques.  Not to mention, your prompt engineering becomes modular and easier to reason about.
+* Calls to LLMs are valuable.
+* Treats multi-modality as first class.
+* Provides tooling using a studio ui.
+
+### Verbose mode
+Use verbose mode activation during initialization to get more details of what happens with the language model invocations
+
+```python
+ell.init(verbose=True)
+```
 
 
-## Get Started
+## Getting Started
+```bash
+# Install your python
+pyenv install 3.10.12
+```
+
+```bash
+# Setup a virtual environment
+pyenv virtualenv 3.10.12 vtidbits
+```
+
+```bash
+# Activate your virtual environment
+pyenv activate vtidbits
+```
+
+```bash
+# Installs `ell` and `ell-studio` on your system allowing you to start using the tools for prompt engineering and visualization.
 pip install -U ell-ai
+```
+
+```bash
+# Verify your installation by checking the version of ell:
+python -c "import ell; print(ell.__version__)"
+```
+
+```python
+# Running the ell-studio
+ell-studio --storage ./logdir
+```
+
+## References
+The following sites are useful for getting started and learning more about ell.
+
+* https://github.com/MadcowD/ell
+* https://docs.ell.so/
+* https://github.com/softwaretidbits/ell
--- ell-1/main.py
+++ ell-1/main.py
@@ -1,6 +1,123 @@
+import os
+import random
 
-# pip install -U ell-ai
+import ell
+import openai
+from PIL import Image
+from dotenv import load_dotenv
+from pydantic import BaseModel, Field
+
+load_dotenv()
+
+# Using openai, but can change this as needed to another provider key
+key = os.getenv('OPENAI_API_KEY')
+assert (key is not None)
+openai_client = openai.Client(api_key=key)
+
+
+class Person(BaseModel):
+    name: str
+    age: int
+    favorite_image: str
+
+
+# Review model
+class TidbitReview(BaseModel):
+    title: str = Field(description="Title of the tidbit")
+    rating: int = Field(description="Rating of the tidbit (1 to 10)")
+    summary: str = Field(description="Summary of the tidbit")
+
+
+# Tidbit for ell container model
+class EllTidbit(BaseModel):
+
+    def hearing(self, name):
+        print(f"=== What are you hearing {name}?")
+        action = self.listen(name)
+        print(action)
+
+    def review(self):
+        print("=== Providing a tidbit review.")
+        review_message = self.create_tidbit_review("An Introduction to ell")
+
+        review = review_message.parsed
+        print(f"Tidbit: {review.title}, Rating: {review.rating}/10")
+        print(f"Summary: {review.summary}")
+
+    def greeting(self, name):
+        print("=== Just say hello.")
+        saying_hello = self.hello(name)
+        print(saying_hello)
+
+    def inspection(self, image_path):
+        print("=== Perform an inspection.")
+        image = Image.open(image_path)
+        description = self.inspect_image(image)
+        print(description)
+
+    def part1(self):
+        print("*** Part 1 Examples")
+        person = Person(name="Mr. Tidbit", age=43, favorite_image="images/cat-image.jpg")
+
+        self.hearing(person.name)
+
+        self.review()
+
+        self.greeting(person.name)
+
+        self.inspection(person.favorite_image)
+
+    @ell.simple(model="gpt-4o-mini", client=openai_client)
+    def listen(self, name: str):
+        """You are a helpful assistant."""
+        sound = self.get_random_noise()
+        return f"{name} is now hearing a {sound}.  What would generate such a sound?"
+
+    @ell.complex(model="gpt-4o-mini", client=openai_client, response_format=TidbitReview)
+    def create_tidbit_review(self, tidbit: str):
+        """You are a tidbit review generator.
+        Given the name of a tidbit, you need to return a structured review."""
+        return f"Generate a review for the tidbit {tidbit}"
+
+    @ell.simple(model="gpt-4o-mini", client=openai_client)
+    def hello(self, name: str):
+        return [
+            ell.system("You are my favorite assistant Babu Frik."),
+            ell.user(f"Say hello to {name}!"),
+            ell.assistant(f"I'd be happy to say hello to {name}"),
+            ell.user("Perfect, I would like to change the greeting to Texan please.")
+        ]
+
+    @ell.simple(model="gpt-4o-mini", client=openai_client)
+    def inspect_image(self, image: Image.Image):
+        return [
+            ell.system("You are my favorite assistant Babu Frik."),
+            ell.user(["What is contained in this image provided?", image])
+        ]
+
+    def get_random_noise(self):
+        noises = ["gulp", "plink", "whistle", "boom"]
+        return random.choice(noises)
+
+
 if __name__ == '__main__':
-    print('Welcome to SoftwareTidbits - Ell (part 1)')
+    print('')
+    print('Welcome to a SoftwareTidbits session.  Using ell for prompt engineering w/ Python')
+    print('')
+    print('What: ')
+    print('An introduction to treating prompts as programs in Python.')
+    print('')
+    print('How: ')
+    print('LMPs are language model programs, representing functions.')
+    print('')
+    print('Get started using ell.  Kicking off some examples now.')
+    print('---')
 
+    # Construct our tidbit
+    ell_tidbit = EllTidbit()
 
+    # Run part 1
+    ell_tidbit.part1()
+
+    # Coming soon, working away on it ...
+    # ell_tidbit.part2()
